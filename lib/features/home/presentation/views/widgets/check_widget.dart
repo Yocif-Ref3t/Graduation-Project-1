5f46a9671b6c4a20c2bffa52c2918374459aa41d
Youssef Refaat <dev.yossifrefaat@gmail.com>
2025-04-15 15:57:13 +0200

    code refactor

---
+++ lib/features/home/presentation/views/widgets/check_widget.dart
@@ -0,0 +1,34 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:graduation_project1/core/extensions/extensions.dart';
+import 'package:graduation_project1/core/widgets/custom_button.dart';
+
+class CheckWidget extends StatelessWidget {
+  const CheckWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      width: double.maxFinite,
+      decoration: BoxDecoration(
+        border: Border.all(color: context.colors.primary),
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Column(
+        spacing: 16,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "CheckYourSelf".tr(context: context),
+            style: context.texts.titleSmall,
+          ),
+          CustomButton(
+            text: "Check".tr(context: context),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,5 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:graduation_project1/core/extensions/extensions.dart';
-import 'package:graduation_project1/core/widgets/custom_button.dart';
+import 'check_widget.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -12,28 +10,7 @@ class HomeViewBody extends StatelessWidget {
       padding: const EdgeInsets.all(16),
       child: Column(
         children: [
-          Container(
-            padding: const EdgeInsets.all(16),
-            width: double.maxFinite,
-            decoration: BoxDecoration(
-              border: Border.all(color: context.colors.primary),
-              borderRadius: BorderRadius.circular(5),
-            ),
-            child: Column(
-              spacing: 16,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "StartExamination".tr(context: context),
-                  style: context.texts.titleSmall,
-                ),
-                CustomButton(
-                  text: "Examination".tr(context: context),
-                  onTap: () {},
-                ),
-              ],
-            ),
-          ),
+          CheckWidget(),
         ],
       ),
     );
